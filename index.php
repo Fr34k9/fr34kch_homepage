4594b2b8e2faabaffe9f3f9d59a6a1203fb4fb66
Fr34k9 <cedric.steiger@gmail.com>
2021-03-10 21:41:15 +0100

    Added plex item
    Added functionality to see active users.

--- index.php
+++ index.php
@@ -13,7 +13,23 @@
             <div class="item6">test</div>
             <div class="item7">test</div>
             <div class="item8">test</div>
-            <div class="item9">test</div>
+            <div class="item9">
+                Plex Item<br>
+			
+			    Active Users: <?php echo plex_getCurrentStreamingUsers(); ?>
+			</div>
         </div>
     </body>
 </html>
+
+<?php
+    function plex_getCurrentStreamingUsers(){
+        $plexUrl = 'http://192.168.1.200:32400';
+        $plexToken = 'X-Plex-Token=nXHeq7DgzAya9yDQMrn5';
+
+        $url = $plexUrl.'/status/sessions?'.$plexToken;
+        $xml = simplexml_load_file($url);
+
+        return sizeof($xml->Video);
+    }
+?>
