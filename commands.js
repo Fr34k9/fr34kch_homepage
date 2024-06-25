b1ab0431ccc3a5786ea066035fdc71ebaef29f0b
Fr34k <cedric.steiger@gmail.com>
2024-06-25 22:50:22 +0200

    Change website to terminal style

---
+++ commands.js
@@ -0,0 +1,15 @@
+const commands = {
+    about: 'This is the main website of Fr34k.ch. I love to code and create new things. I am always open to new ideas and projects. Feel free to contact me anytime.\n' + 
+            'Github: <a href="https://github.com/fr34k9" target="_blank" class="text-blue-300 text-underline">github.com/fr34k9</a>\n' + 
+            'LinkedIn: <a href="https://linkedin.com/in/cedric-steiger/" target="_blank" class="text-blue-300 text-underline">linkedin.com</a>\n' +
+            'Email: <a href="mailto:admin@fr34k.ch" class="text-blue-300 text-underline">admin@fr34k.ch</a>\n' +
+            'Website: <a href="https://csteiger.ch" target="_blank" class="text-blue-300 text-underline">csteiger.ch</a>',
+    projects: 'Some of my latest projects:\n' +
+                '<a href="https://deals.fr34k.ch" target="_blank" class="text-blue-300 text-underline">deals.fr34k.ch</a><span class="text-gray-500"> # Best Deals from Switzerland</span>\n' +
+                '<a href="https://notifier.fr34k.ch" target="_blank" class="text-blue-300 text-underline">notifier.fr34k.ch</a><span class="text-gray-500"> # Simplify Notifications</span>\n' + 
+                '<a href="https://gitboost.fr34k.ch" target="_blank" class="text-blue-300 text-underline">gitboost.fr34k.ch</a><span class="text-gray-500"> # Boost your github profile</span>',
+    help: 'Available commands: ' + 
+            'about, projects, repo, help, clear',
+    repo: 'Find the source code of this project on <a href="https://github.com/Fr34k9/fr34kch_homepage" target="_blank" class="text-blue-300 text-underline">github.com/Fr34k9/fr34kch_homepage</a>',
+    clear: ''
+};
